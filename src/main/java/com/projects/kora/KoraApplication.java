14ed9dba0a76b5d29870cd8e216e70c947e98f6c
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Pro.local>
2021-07-07 11:43:47 +0530

    Created new project

---
+++ src/main/java/com/projects/kora/KoraApplication.java
@@ -0,0 +1,13 @@
+package com.projects.kora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoraApplication.class, args);
+	}
+
+}
