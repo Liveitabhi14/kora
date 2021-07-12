76826e67c6f232fc83760f4997aa8362b7984051
mithleshkumarsah <itsmithlesh>
2021-07-12 18:20:10 +0530

    Top 5 answer of each question api added and see all answer of question updated

--- src/main/java/com/projects/kora/repository/QuestionRepository.java
+++ src/main/java/com/projects/kora/repository/QuestionRepository.java
@@ -5,5 +5,8 @@ import com.projects.kora.design.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
+    List<Question> findByQuesId(Integer quesId);
 }
