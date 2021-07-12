a255884e30672095e540cea4ecdea70dda5e65fd
mithleshkumarsah <itsmithlesh>
2021-07-12 18:41:32 +0530

    warning fixed

--- src/main/java/com/projects/kora/repository/AnswerRepository.java
+++ src/main/java/com/projects/kora/repository/AnswerRepository.java
@@ -4,7 +4,6 @@ package com.projects.kora.repository;
 import com.projects.kora.design.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
--- src/main/java/com/projects/kora/repository/QuestionRepository.java
+++ src/main/java/com/projects/kora/repository/QuestionRepository.java
@@ -3,7 +3,7 @@ package com.projects.kora.repository;
 
 import com.projects.kora.design.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
 
 import java.util.List;
 
